3a49de139f8e374a9961bfd88b7ee6298463c142
Joe Ebmeier <joe.ebmeier@firespring.com>
2020-02-24 12:13:14 -0600

    WIP

--- lib/docker/util.rb
+++ lib/docker/util.rb
@@ -276,4 +276,182 @@ module Docker::Util
     patterns.map! { |p| File.directory?(p) ? "#{p}/**/*" : p }
     patterns.flat_map { |p| p =~ GLOB_WILDCARDS ? glob_all_files(p) : p }
   end
+
+  def container_copy(container, source_path, dest_path, required: true)
+    LOG.info "Copying #{source_path} to #{dest_path}... "
+
+    tar = StringIO.new
+    begin
+      container.copy(source_path) do |chunk|
+        tar.write(chunk)
+      end
+    rescue => e
+      raise e if required
+      puts "Not Found"
+    end
+    tar.rewind
+
+    extended_headers = nil
+    reader = Gem::Package::TarReader.new(tar)
+    reader.each do |entry|
+      # Using https://github.com/kr/tarutil/blob/master/untar.go as a template
+      # Also check out https://go.googlesource.com/go/+/master/src/archive/tar/reader.go?autodive=0%2F%2F%2F
+      case entry.header.typeflag
+      when TarTypeFlag::TYPE_DIR
+        merge_pax(entry, extended_headers)
+        dest_name = calc_dest_name(source_path, dest_path, entry)
+        create_directory(entry, dest_name)
+
+      when TarTypeFlag::TYPE_REG, TarTypeFlag::TYPE_REG_A
+        merge_pax(entry, extended_headers)
+        dest_name = calc_dest_name(source_path, dest_path, entry)
+        create_file(entry, dest_name)
+
+      when TarTypeFlag::TYPE_LINK
+        raise 'Unimplemented file type: Link'
+
+      when TarTypeFlag::TYPE_SYMLINK
+        merge_pax(entry, extended_headers)
+        dest_name = calc_dest_name(source_path, dest_path, entry)
+        create_symlink(entry, dest_name)
+
+      when TarTypeFlag::TYPE_X_HEADER
+        extended_headers = parse_pax(entry.read)
+        next
+
+      when TarTypeFlag::TYPE_CONT, TarTypeFlag::TYPE_X_GLOBAL_HEADER
+        raise 'Unimplemented file type Cont/XGlobalHeader'
+
+      when TarTypeFlag::TYPE_CHAR, TarTypeFlag::TYPE_BLOCK, TarTypeFlag::TYPE_FIFO
+        raise 'Unimplemented file type: Char/Block/Fifo'
+
+      else
+        raise 'Unrecognized file type'
+
+      end
+
+      # If we got here we should be done with any extended headers
+      extended_headers = nil
+    end
+  end
+
+  # NOTE: These methods are most likely not complete
+  def parse_pax(content)
+    extended_headers = {}
+    key, value = parse_pax_record(content)
+    extended_headers[key] = value
+    extended_headers
+  end
+
+  def parse_pax_record(content)
+    # Check https://golang.org/src/archive/tar/strconv.go
+    size, keyvalue = content&.split(' ', 2)
+    key, value = keyvalue&.split('=', 2)
+    [key, value]
+  end
+
+  def calc_dest_name(source_path, dest_path, entry)
+    dest_path = dest_path.to_s.strip.chomp('/')
+    return dest_path unless File.directory?(dest_path)
+
+    source_path = source_path.to_s.strip.chomp('/')
+    full_name = entry.full_name.to_s.strip
+    full_name = full_name.sub(%r{^#{source_path}/}, '')
+    "#{dest_path.chomp('/')}/#{full_name}"
+  end
+
+  def create_directory(entry, dest_name)
+    FileUtils.mkdir_p(dest_name)
+  end
+
+  def create_file(entry, dest_name)
+    FileUtils.mkdir_p(File.dirname(dest_name))
+    IO.write(dest_name, entry.read)
+  end
+
+  def create_symlink(entry, dest_name)
+    FileUtils.cd(File.dirname(dest_name)) {
+      FileUtils.mkdir_p(File.dirname(entry.header.linkname))
+      FileUtils.symlink(entry.header.linkname, File.basename(dest_name))
+    }
+  end
+
+  def merge_pax(entry, extended_headers)
+    # Reference: https://go.googlesource.com/go/+/master/src/archive/tar/reader.go?autodive=0%2F%2F%2F
+    return unless extended_headers
+
+    extended_headers.each do |k, v|
+      case k
+      when PaxHeader::PAX_PATH
+        entry.header.instance_variable_set(:@name, v)
+
+      when PaxHeader::PAX_LINKPATH
+        entry.header.instance_variable_set(:@linkname, v)
+
+      when PaxHeader::PAX_UNAME
+        entry.header.instance_variable_set(:@uname, v)
+
+      when PaxHeader::PAX_GNAME
+        entry.header.instance_variable_set(:@gname, v)
+
+      when PaxHeader::PAX_UID
+        entry.header.instance_variable_set(:@uid, v)
+
+      when PaxHeader::PAX_GID
+        entry.header.instance_variable_set(:@gid, v)
+
+      when PaxHeader::PAX_ATIME
+        entry.header.instance_variable_set(:@atime, v)
+
+      when PaxHeader::PAX_MTIME
+        entry.header.instance_variable_set(:@mtime, v)
+
+      when PaxHeader::PAX_CTIME
+        entry.header.instance_variable_set(:@ctime, v)
+
+      when PaxHeader::PAX_SIZE
+        entry.header.instance_variable_set(:@size, v)
+
+      else
+        raise "unsupported header #{k}"
+
+      end
+    end
+  end
+
+  # From https://golang.org/src/archive/tar/common.go?s=5701:5766
+  module TarTypeFlag
+    TYPE_REG             = '0'    # regular file
+    TYPE_REG_A           = '\x00' # regular file
+    TYPE_LINK            = '1'    # hard link
+    TYPE_SYMLINK         = '2'    # symbolic link
+    TYPE_CHAR            = '3'    # character device node
+    TYPE_BLOCK           = '4'    # block device node
+    TYPE_DIR             = '5'    # directory
+    TYPE_FIFO            = '6'    # fifo node
+    TYPE_CONT            = '7'    # reserved
+    TYPE_X_HEADER        = 'x'    # extended header
+    TYPE_X_GLOBAL_HEADER = 'g'    # global extended header
+    TYPE_GNU_LONG_NAME   = 'L'    # Next file has a long name
+    TYPE_GNU_LONG_LINK   = 'K'    # Next file symlinks to a file w/ a long name
+    TYPE_GNU_SPARSE      = 'S'    # sparse file
+  end
+
+  module PaxHeader
+    PAX_ATIME   = "atime"
+    PAX_CHARSET = "charset"
+    PAX_COMMENT = "comment"
+    PAX_CTIME   = "ctime" # please note that ctime is not a valid pax header.
+    PAX_GID     = "gid"
+    PAX_GNAME   = "gname"
+    PAX_LINKPATH= "linkpath"
+    PAX_MTIME   = "mtime"
+    PAX_PATH    = "path"
+    PAX_SIZE    = "size"
+    PAX_UID     = "uid"
+    PAX_UNAME   = "uname"
+    PAX_XATTR   = "SCHILY.xattr."
+    PAX_NONE    = ""
+  end
+
 end
